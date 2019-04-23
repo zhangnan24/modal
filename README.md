727e62c4f2726e00d343ed3d690df3297f587a6a
zhangnan <zhangnan35@foxmail.com>
2019-04-23 10:52:21 +0800

    添加了README和*.vue文件

---
+++ README.md
@@ -0,0 +1,23 @@
+# modal
+
+# 说明
+这是一个手撸的*.vue组件，极简主义代码，不是通用性的，可以自己改样式
+
+# 使用方法
+在父组件中传入visible属性直接控制modal显隐，接受自定义方法hide隐藏modal
+示例:
+```父组件
+    <modal :visible="modalShow" @hide="modalShow = $event">
+      <div>这是插槽内容</div>
+    </modal>
+```
+# 项目解析
+- 为什么不取名为dialog? --避免冲突，dialog是一个原生HTML5元素，尽管现在支持度不佳
+- 为什么style的样式要包裹在.modal下？ --为modal组件的样式创建命名空间，避免与父组件样式的冲突
+- @click.stop是什么操作？ --阻止事件冒泡
+- dialog自身没有数据的？ --既然要完全由父组件来控制子组件的显隐，当然只需要依托唯一布尔值即可
+
+# 注意事项
+- 父组件中不应有html元素的class名为modal,否则会引起样式冲突
+- 传入的title不是必须的，但visible是必须的，否则会报错
+- 插槽的内容置于modal的body部分
---
+++ modal.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="modal" v-show="visible" @click.stop="hideSelf">
+    <div class="center" @click.stop>
+      <div class="head">
+        {{ title ? title : ''}}
+        <button @click="hideSelf"></button>
+      </div>
+      <div class="body">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+    title: String,
+  },
+  methods: {
+    hideSelf() {
+      this.$emit('hide', false);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .center {
+    width: 1036px;
+    height: 818px;
+
+    .head {
+      height: 50px;
+      background: #fb621e;
+      color:#fff;
+      font-size: 20px;
+      text-align: center;
+      line-height:50px;
+      position: relative;
+
+      button {
+        position: absolute;
+        right: 15px;
+        top: 8px;
+        display: block;
+        width: 34px;
+        height: 34px;
+        border: 0;
+        padding: 0;
+        outline: none;
+        cursor: pointer;
+        background: url(../../../public/img/dialogBtn.png) no-repeat;
+        transition: transform .2s;
+      }
+
+      button:hover{
+        transform: rotateZ(180deg);
+      }
+    }
+
+    .body {
+      height: 748px;
+      background: #fff;
+      overflow: hidden;
+    }
+  }
+}
+</style>
